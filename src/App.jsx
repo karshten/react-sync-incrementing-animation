ce9613017157a11e35422c9d2f6a01d160846116
karshten <oskar.shtefan@gmail.com>
2022-12-13 15:42:53 +0600

    added some conditions for incrementing numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useEffect, useRef } from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 
-const numbers = [100, 10]
+const numbers = [400, 225, 11, 1500]
 function App() {
   const [incrementing, setIncrementing] = useState(false)
   const refs = useRef({})
 
-
   for (let i = 0; i < Object.values(refs.current).length; i++) {
     let start = 0
     const element = refs.current[i];
+    
+    const duration = 500
 
-    const duration = 2000
     const interval = Math.floor(duration / numbers[i])
 
     const counter = setInterval(() => {
-      start += 1
-      element.innerText = start
+      let num = 1
 
-      if(start === numbers[i]) clearInterval(counter)
+      if(numbers[i] >= 1000) num = 10
+      if(numbers[i] <= 500) num = 5
+      if(numbers[i] < 100) num = 1
+
+
+      start += num
+      element.textContent = start
+
+      if(start >= numbers[i]) clearInterval(counter)
     }, interval)
   };
 
@@ -29,6 +36,12 @@ function App() {
       </div>
       <div className='card'>
         <p ref={elem => refs.current[1] = elem} data={0}>0</p>
+      </div>
+      <div className='card'>
+        <p ref={elem => refs.current[2] = elem} data={0}>0</p>
+      </div>
+      <div className='card'>
+        <p ref={elem => refs.current[3] = elem} data={0}>0</p>
       </div>
 
       <div className='btn' ><button onClick={e => setIncrementing(true)}>set</button></div>
